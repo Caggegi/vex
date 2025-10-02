1e99fff3b34af4f6e3bf71e68961be791121c31c
Caggegi <rosariocaggegi99@gmail.com>
2025-10-02 22:26:22 +0200

    feat(tracking, settings, administration): add tracking skeleton, add admin page, add settings page

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,14 +1,26 @@
 "use client"
 import Image from "next/image";
 import styles from "./navbar.module.css";
-import { Avatar, TabNav } from "@radix-ui/themes";
+import { Avatar, Button, DropdownMenu, TabNav } from "@radix-ui/themes";
 import { useTranslations } from 'next-intl';
 import { usePathname } from "next/navigation";
+import Icon from "@mdi/react";
+import { mdiCog, mdiLogout, mdiSettingsHelper, mdiShieldCrown } from "@mdi/js";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 
 
 export default function Navbar() {
     const t = useTranslations('Navbar');
+    const [user, setUser] = useState<any>({admin:false})
+
+    useEffect(()=>{
+        let user = localStorage.getItem("user")
+        if(user)
+            setUser(JSON.parse(user))
+    }, [])
+
     return (
         <div className={styles.navbar_layout}>
             <Image
@@ -21,12 +33,26 @@ export default function Navbar() {
             />
             <div className={styles.navbar_actions}>
                 <TabNav.Root>
-                    <TabNav.Link href="/" active={usePathname()==="/"} className={styles.link}>
+                    <TabNav.Link href="/" active={usePathname() === "/"} className={styles.link}>
                         {t('send')}
                     </TabNav.Link>
-                    <TabNav.Link href="/tracking" active={usePathname()==="/tracking/"}  className={styles.link}>{t('tracking')}</TabNav.Link>
+                    <TabNav.Link href="/tracking" active={usePathname() === "/tracking/"} className={styles.link}>{t('tracking')}</TabNav.Link>
                 </TabNav.Root>
-                <Avatar fallback="RC" style={{ margin: "0 8px" }} />
+                <DropdownMenu.Root>
+                    <DropdownMenu.Trigger>
+                        <div>
+                            <Avatar fallback="RC" style={{ margin: "0 8px" }} />
+                        </div>
+                    </DropdownMenu.Trigger>
+                    <DropdownMenu.Content>
+                        <Link href="/settings"><DropdownMenu.Item><Icon path={mdiCog} size={0.8}/>Settings</DropdownMenu.Item></Link>
+                        {(user && user.admin) && <Link href="/administration"><DropdownMenu.Item><Icon path={mdiShieldCrown} size={0.8}/>Administration</DropdownMenu.Item></Link>}
+                        <DropdownMenu.Separator />
+                        <Link href="/logout"><DropdownMenu.Item color="red">
+                            <Icon path={mdiLogout} size={0.8}/>Logout
+                        </DropdownMenu.Item></Link>
+                    </DropdownMenu.Content>
+                </DropdownMenu.Root>
             </div>
         </div>
     )
